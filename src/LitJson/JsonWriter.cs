0c763169bb75839ee5fbae3fb50a777422ad91bc
swanky <swanky.wu@gmail.com>
2020-06-18 17:04:26 +0800

    can write float;remove the attribute;

--- src/LitJson/JsonWriter.cs
+++ src/LitJson/JsonWriter.cs
@@ -339,6 +339,21 @@ namespace LitJson
             context.ExpectingValue = false;
         }
 
+        public void Write(float number)
+        {
+            DoValidation(Condition.Value);
+            PutNewline();
+
+            string str = Convert.ToString(number, number_format);
+            Put(str);
+
+            if (str.IndexOf('.') == -1 &&
+                str.IndexOf('E') == -1)
+                writer.Write(".0");
+
+            context.ExpectingValue = false;
+        }
+
         public void Write (int number)
         {
             DoValidation (Condition.Value);
@@ -372,7 +387,7 @@ namespace LitJson
             context.ExpectingValue = false;
         }
 
-        [CLSCompliant(false)]
+        // [CLSCompliant(false)]
         public void Write (ulong number)
         {
             DoValidation (Condition.Value);
